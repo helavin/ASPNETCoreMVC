7a9ef58a968ba73f13f93a7d61bddbd2d5e23b7e
helavin <helavin@yandex.ru>
2020-09-21 11:27:40 +0300

    Add project files.

---
+++ DataLayer/EFDBContext.cs
@@ -0,0 +1,30 @@
+﻿using DataLayer.Entities;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayer
+{
+    public class EFDBContext : DbContext
+    {
+        public DbSet<Directory> Directory { get; set; }
+        public DbSet<Material> Material { get; set; }
+
+        public EFDBContext(DbContextOptions<EFDBContext> options) : base(options) { }
+    }
+
+    public class EFDBContextFactory : IDesignTimeDbContextFactory<EFDBContext>
+    {
+        public EFDBContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<EFDBContext>();
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=ASPCoreDB;Trusted_Connection=True;MultipleActiveResultSets=true",
+                b => b.MigrationsAssembly("DataLayer"));
+            return new EFDBContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ DataLayer/Entities/Material.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayer.Entities
+{
+    public class Material : Page
+    {
+        public int DirectoryId { get; set; } // внешний ключ
+        public Directory Directory { get; set; } // навигационное свойство
+    }
+}
